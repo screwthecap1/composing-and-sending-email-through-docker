8cd4f14a8fcfca219e6c78ac4f502446aa7bc9cd
Klim Borodin <123664193+screwthecap1@users.noreply.github.com>
2024-01-02 20:23:06 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from email.message import EmailMessage
-import smtplib
+import smtplib  # отправка сообщения
+from string import Template
+from pathlib import Path
 
 my_email = EmailMessage()
 
+html_template = Template(Path("templates/index.html").read_text())
+html_content = html_template.substitute({'name': 'Klim', 'date': 'next week'})
+
 my_email['from'] = 'Klim'
-my_email['to'] = 'chanskiy777@mail.ru'
-my_email['subject'] = 'Hello from Python'
-my_email.set_content("Hey! How are you, bro?")
+my_email['to'] = 'bro@mail.ru'
+my_email['subject'] = 'I wanna make it for real'
+my_email.set_content(html_content, 'html')
 
 with smtplib.SMTP(host='localhost', port=2525) as smtp_server:
     smtp_server.ehlo()
